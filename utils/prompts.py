2502d1b44b28af1bf0bc313a4ade6b4f54391e0d
rpcvjet <kenneth.ashley@gmail.com>
2025-06-23 11:12:42 -0400

    fixing path to absolute

--- utils/prompts.py
+++ utils/prompts.py
@@ -1,5 +1,7 @@
+from pathlib import Path
+
 def build_scene_prompt(char1, char2, sample_lines):
-    with open("prompt_template.txt", "r") as f:
+    path = Path(__file__).parent / "prompt_template.txt"
+    with open(path, "r", encoding="utf-8") as f:
         template = f.read()
     return template.format(char1=char1, char2=char2, sample_lines=sample_lines)
-
